8bb4b64f727fb4934c40e21667f133aae3b28f09
Alex Grobovsky <a.grobovsky@gmail.com>
2023-10-01 19:51:46 +0500

    add clerk auth

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,8 +1,8 @@
-
+import { UserButton } from '@clerk/nextjs';
 export default function Home() {
   return (
     <div>
-      This is protected page
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
